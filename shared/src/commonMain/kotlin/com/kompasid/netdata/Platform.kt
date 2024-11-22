29c0adae617132a3131fb0b95e0cf7c183a016ba
nurirppan <nurirppan@gmail.com>
2024-11-22 16:01:39 +0700

    - remove all

--- shared/src/commonMain/kotlin/com/kompasid/netdata/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kompasid.netdata
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- shared/src/iosMain/kotlin/com/kompasid/netdata/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.kompasid.netdata
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
