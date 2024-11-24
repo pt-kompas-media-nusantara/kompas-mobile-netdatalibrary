a3c4bac14ac8d8dcbe3e05d9dfa65d7630c7eedf
nurirppan <nurirppan@gmail.com>
2024-11-24 17:46:54 +0700

    ! coba pakai branch

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.9
-
 import PackageDescription
 
 let package = Package(
@@ -16,7 +15,14 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            path: "./Shared.xcframework"
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/{version}/Shared.xcframework.zip",
+            checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd"
+        ),
+        // Jika ingin menggunakan branch (misalnya "main" atau "dev")
+        .binaryTarget(
+            name: "Shared",
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/archive/refs/heads/Shared.xcframework.zip",
+            checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd" // "checksum_for_main_branch" // Ganti dengan checksum yang sesuai
         )
     ]
 )
