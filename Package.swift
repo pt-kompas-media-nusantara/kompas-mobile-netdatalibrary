ad30902466e85c4286fe2819af7c29105ba178ba
nurirppan <nurirppan@gmail.com>
2024-11-24 17:48:49 +0700

    ! coba pakai static branch aja

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/{version}/Shared.xcframework.zip",
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.5/Shared.xcframework.zip",
             checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd"
         ),
         // Jika ingin menggunakan branch (misalnya "main" atau "dev")
