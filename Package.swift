8d61a9e6f4840ab13e23a9b459f31aeaa860c290
nurirppan <nurirppan@gmail.com>
2024-11-24 17:28:03 +0700

    coba lagi

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:5.9
+
 import PackageDescription
 
 let package = Package(
@@ -15,12 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.3/Shared.xcframework.zip",
-            checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd"
-        ),
-        .target(
-            name: "Dummy",
-            path: "Sources/Dummy" // Tentukan lokasi file sumber
+            path: "./Shared.xcframework"
         )
     ]
 )
