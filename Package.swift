e817c0bd4261f03fb262ca37d22567581f93f382
nurirppan <nurirppan@gmail.com>
2024-11-25 09:27:46 +0700

    + tambah add

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.7/Shared.xcframework.zip",
-            checksum: "2f164e3eee315002ef6bb43833247a320cf5db051d6942482f2b75f6599d44d6"
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.8/Shared.xcframework.zip",
+            checksum: "5e8c59dc1d247480fa112b2ca72717d960ce791771441ccbafed313c603eb6f0"
         ),
         .target(
             name: "Dummy",
