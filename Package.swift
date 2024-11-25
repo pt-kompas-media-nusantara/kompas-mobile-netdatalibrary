43c9c21f2f6a6292cb4d3e50421d0b5294714fc2
nurirppan <nurirppan@gmail.com>
2024-11-25 08:02:53 +0700

    ! coba 1.0.6

--- Package.swift
+++ Package.swift
@@ -15,14 +15,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.5/Shared.xcframework.zip",
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.6/Shared.xcframework.zip",
             checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd"
         ),
-        // Jika ingin menggunakan branch (misalnya "main" atau "dev")
-        .binaryTarget(
-            name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/archive/refs/heads/Shared.xcframework.zip",
-            checksum: "cb037a2d79ce75e66a5e538f6c0268add0259810f82a7ae04e068e36ac998ebd" // "checksum_for_main_branch" // Ganti dengan checksum yang sesuai
+        .target(
+            name: "Dummy",
+            path: "Sources/Dummy" // Tentukan lokasi file sumber
         )
     ]
 )
