cea00383620fa9c6b38391772b9af9a0dd2e83d2
wahyukharismaa <made.kirana@kompasid.dev>
2025-02-18 14:52:28 +0800

    ! update publisher

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -102,7 +102,7 @@ kotlin {
 
 publishing {
     publications {
-        create<MavenPublication>("default") {
+        create<MavenPublication>("shared") {
             from(components["kotlin"])
             groupId = "com.kompasid.netdatalibrary"
             artifactId = "shared"
