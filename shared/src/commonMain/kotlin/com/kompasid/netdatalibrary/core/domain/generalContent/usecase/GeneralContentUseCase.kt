02e492110dce46adbcb8b16573c382e1a8338a35
wahyukharismaa <made.kirana@kompas.com>
2025-02-26 12:21:43 +0800

    Refactor GeneralContentUseCase class definition
    - Changed `repository` property modifier from `val` to `private val` in `GeneralContentUseCase`.

--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/domain/generalContent/usecase/GeneralContentUseCase.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/domain/generalContent/usecase/GeneralContentUseCase.kt
@@ -5,7 +5,7 @@ import com.kompasid.netdatalibrary.base.network.Results
 import com.kompasid.netdatalibrary.core.domain.generalContent.interceptor.GeneralContentInterceptor
 import com.kompasid.netdatalibrary.core.data.generalContent.repository.IGeneralContentRepository
 
-class GeneralContentUseCase(val repository: IGeneralContentRepository) : IGeneralContentUseCase {
+class GeneralContentUseCase(private val repository: IGeneralContentRepository) : IGeneralContentUseCase {
     override suspend fun getGeneralData(customUrl: String): Results<GeneralContentInterceptor, NetworkError> {
         return repository.getGeneralData(customUrl)
     }
