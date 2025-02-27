90c1979e3056cf2a10e2985fd2ffb2d7ed6314b3
nurirppan <nurirppan@gmail.com>
2025-02-27 16:17:41 +0700

    Update Package.swift and Shared.xcframework.zip for version 1.0.59

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.58/Shared.xcframework.zip",
-            checksum: "558c4c91943079b684761fa8db9fce168d90f51682d5f19f0ca3bc136bbb014f"
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.59/Shared.xcframework.zip",
+            checksum: "2244116f1f78c05564421645f13d920ba26b6923b83a5c6c185b076a6c32048d"
         ),
         .target(
             name: "Dummy",
