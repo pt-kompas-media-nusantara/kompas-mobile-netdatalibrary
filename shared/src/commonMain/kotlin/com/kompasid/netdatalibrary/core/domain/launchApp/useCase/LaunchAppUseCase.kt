8bd413309ba3e9593dff626945389bc6f9d76818
nurirppan <nurirppan@gmail.com>
2025-04-17 12:01:39 +0700

    ! perbaikan device info

--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/domain/launchApp/useCase/LaunchAppUseCase.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/domain/launchApp/useCase/LaunchAppUseCase.kt
@@ -11,16 +11,18 @@ class LaunchAppUseCase(
     private val settingsHelper: SettingsHelper
 ) {
     suspend fun execute(data: LaunchAppInterceptor) = coroutineScope {
+        // "(\(UIDevice.current.systemName)) Native |  \(UIDevice.current.name) | \(UIDevice.current.model) | \(UIDevice.current.systemVersion) | Get From Library"
+        val device: String = "(${data.deviceInfoState.uiDeviceSystemName}) Native |  ${data.deviceInfoState.uiDeviceName} | ${data.deviceInfoState.uiDeviceModel} | ${data.deviceInfoState.osVersion}| ${data.deviceInfoState.uiDeviceSeries}"
         listOf(
             // DeviceInfoState
 
             // hardcode sementara
-            async { settingsHelper.save(KeySettingsType.IS_FIRST_INSTALL, false) },
-            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_SYSTEM_NAME, "concate string") },
-            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_NAME, "concate string") },
-            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_MODEL, "concate string") },
-            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_SERIES, "concate string") },
-            async { settingsHelper.save(KeySettingsType.DEVICE, "concate string") },
+            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_SYSTEM_NAME, data.deviceInfoState.uiDeviceSystemName) },
+            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_NAME, data.deviceInfoState.uiDeviceName) },
+            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_MODEL, data.deviceInfoState.uiDeviceModel) },
+            async { settingsHelper.save(KeySettingsType.IOS_UI_DEVICE_SERIES, data.deviceInfoState.uiDeviceSeries) },
+
+            async { settingsHelper.save(KeySettingsType.DEVICE, device) },
 
             async { settingsHelper.save(KeySettingsType.DEVICE_TYPE, data.deviceInfoState.deviceType.value) },
             async { settingsHelper.save(KeySettingsType.OS_VERSION, data.deviceInfoState.osVersion) },
--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceInfoState.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceInfoState.kt
@@ -1,29 +1,17 @@
 package com.kompasid.netdatalibrary.core.presentation.launchApp.model
 
+import com.kompasid.netdatalibrary.helper.persistentStorage.KeySettingsType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 @Serializable
 data class DeviceInfoState(
+    val uiDeviceSystemName: String = "",
+    val uiDeviceName: String = "",
+    val uiDeviceModel: String = "",
+    val uiDeviceSeries: String = "",
     val device: String = "",
     val deviceType: DeviceType = DeviceType.SMARTPHONE,
-    val docReferrer: String = "",
     val osVersion: String = "",
     val currentVersionApp: String = "",
-    val newVersionApp: String = "",
-){
-    fun toJson(): String {
-        return Json.encodeToString(
-            serializer(), DeviceInfoState(
-                device, deviceType, docReferrer, osVersion, currentVersionApp, newVersionApp
-            )
-        )
-    }
-
-    companion object {
-        fun fromJSON(data: String): DeviceInfoState {
-            return Json.decodeFromString(data)
-        }
-    }
-}
-
+)
