4168501800ca8134e1c49aab2cd5cee4733b1a08
nurirppan <nurirppan@gmail.com>
2025-04-22 17:23:21 +0700

    - hapus device param

--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceInfoState.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceInfoState.kt
@@ -8,7 +8,6 @@ data class DeviceInfoState(
     val uiDeviceName: String, // John's iPhone
     val uiDeviceModel: String, // iPhone
     val uiDeviceSeries: String, // iPhone 6 Plus
-    val device: String, // (iOS) Native | iPhone | iPhone | 16.7.8 | iPhone 6 Plus
     val deviceType: DeviceType, // smartphone, tablet, phablet, desktop
     val osVersion: String, // 17.4
     val currentAppVersionKompasId: String, // 3.50.0
