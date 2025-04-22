05daf4b860758590957857f072cac63953010dda
nurirppan <nurirppan@gmail.com>
2025-04-22 16:54:48 +0700

    ! remove initial value for launch app usecase

--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/ConfigurationSystemState.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/ConfigurationSystemState.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 data class ConfigurationSystemState(
-    val flavors: String = "", // DEBUG, ALLCLOUD, DKID_ID, DKID_CLOUD, DQA_ID, DQA_CLOUD, RQA_ID, RKID_ID
-    val isDebug: Boolean = false, // true or false
-    val isLogActived: Boolean = false, // true or false
+    val flavors: String, // DEBUG, ALLCLOUD, DKID_ID, DKID_CLOUD, DQA_ID, DQA_CLOUD, RQA_ID, RKID_ID
+    val isDebug: Boolean, // true or false
+    val isLogActived: Boolean, // true or false
 )
--- shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceSubcriptionState.kt
+++ shared/src/commonMain/kotlin/com/kompasid/netdatalibrary/core/presentation/launchApp/model/DeviceSubcriptionState.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 data class DeviceSubcriptionState(
-    val historyTransaction: String = "", // originalTransactionId & transactionId akan di ambil dari historyTransaction (convert sendiri)
+    val historyTransaction: String, // originalTransactionId & transactionId akan di ambil dari historyTransaction (convert sendiri)
 )
