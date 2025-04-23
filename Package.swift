44981fa774ad274624bed100f7849ac9126f45d4
nurirppan <nurirppan@gmail.com>
2025-04-23 14:25:53 +0700

    Update Package.swift and KompasIdLibrary.xcframework.zip for version 1.0.71

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KompasIdLibrary",
-            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.70/KompasIdLibrary.xcframework.zip",
-            checksum: "3b6d85b9525d696fadbbde3699ed8b6b013c77bb4f96ddcee7a5120bc238adae"
+            url: "https://github.com/pt-kompas-media-nusantara/kompas-mobile-netdatalibrary/releases/download/1.0.71/KompasIdLibrary.xcframework.zip",
+            checksum: "f62c46f95145972b45ac29d6f717fd1c7121dc7126e8538cf1f0a52b3f23ac80"
         ),
         .target(
             name: "Dummy",
